2b03fb20bd0917d34c841ff80cb13522752fbdff
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-06-03 08:44:06 +0530

    button is implicitly button

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -4,7 +4,7 @@ export default function Topbar(props){
 return  <>
         <nav class="navbar navbar-dark bg-dark">
         <a class="navbar-brand" href="index.html"><Svgtopbar/>OurLogger</a>
-        {props.loginStatus?<button className="nav-link dropdown-toggle" href=""  id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        {props.loginStatus?<button className="nav-link dropdown-toggle" href=""  id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <img src="https://img.icons8.com/bubbles/75/000000/user-male.png" alt="icon8"/>
         </button>:<></>}
         <div class="dropdown-menu" style={{marginLeft:"auto"}} aria-labelledby="navbarDropdownMenuLink">
