938a0267c2cfdeb6705bd0efff38c9896559c735
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-06-04 12:03:57 +0530

    newer implementation

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,6 +1,6 @@
 
 import {useParams} from 'react-router-dom'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 
 
 export default function ResetPassword(){
@@ -8,9 +8,14 @@ export default function ResetPassword(){
     let { str } = useParams();
     const [urlstring,setUrlstring] = useState()
     const [user,setUser] = useState({})
-    setUrlstring(str)
-        fetch(`https://blissful-rosalind-539ba2.netlify.app/${urlstring}`)
-        .then(response=>response.json())
-        .then(data=>setUser(data))
+   
+    
+     const exec=()=>{fetch(`https://blissful-rosalind-539ba2.netlify.app/${urlstring}`)
+     .then(response=>response.json())
+     .then(data=>setUser(data))}   
+     useEffect(()=>{
+        setUrlstring(str)
+        exec()
+    })
     return <div> Now shing the following page {str}<div>{JSON.stringify(user)}</div></div>
   }
