2c17758bb60583861604883e43864ab438b83514
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-06-04 11:48:41 +0530

    removing useeffect function

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -9,12 +9,10 @@ export default function ResetPassword(){
     const [urlstring,setUrlstring] = useState()
     const [user,setUser] = useState({})
     setUrlstring(str)
-    useEffect(()=>{
-       
         fetch(`https://blissful-rosalind-539ba2.netlify.app/${urlstring}`)
         .then(response=>response.json())
         .then(data=>setUser(data))
-    })
+   
 
 
     return <div> Now shing the following page {str}<div>{JSON.stringify(user)}</div></div>
