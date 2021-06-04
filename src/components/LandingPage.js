2be85ca7ba3b4872bdf82b669ea2c2456c9c3bba
KowshikEPatel <69798483+KowshikEPatel@users.noreply.github.com>
2021-06-04 23:46:06 +0530

    success landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -18,7 +18,9 @@ export default function LandingPage(){
 
     const [username,setUsername] = useState("")
     const [passwords,setPasswords] = useState("")
-
+    const [displayvalue,setDisplayvalue]=useState('block')
+    const [displaysuccess,setDisplaysuccess] = useState('none')
+    const [authUser,setauthUser] = useState('')
     function handleChangeUser(event){
         setUsername(event.target.value)
     }
@@ -26,9 +28,30 @@ export default function LandingPage(){
         setPasswords(event.target.value)
      }
 
+     function getuser(){
+         fetch('',{
+             method:'POST',
+             headers:{
+                'Content-Type': 'application/json'
+             },
+             body:JSON.stringify({
+                 "user_name":username,
+                 "password":password
+             })
+         })
+         .then(response=>{
+             console.log(response.status)
+             response.blob().then(data=>{
+                setauthUser(data["user"])
+                setDisplayvalue('none')
+                setDisplaysuccess('block')
+             })
+         })
+         
+     }
 
     return  <>
-        <div className="card" style={{width:"20rem",margin:"70px 0 0 400px"}}>
+        <div className="card" style={{width:"20rem",margin:"70px 0 0 400px",display:displayvalue}}>
              <div class="card-body">
                      <h4 class="card-title">Login to your account</h4>
                    <h5>Username</h5>
@@ -36,11 +59,16 @@ export default function LandingPage(){
                    <h5>Password</h5>
                    <input type="password" width="100px" className="form-control" style={{marginBottom:"15px"}} onChange={handleChangePassword} value={passwords}></input>
                    <Link to='/forgotpw'><small> Forgot password? </small></Link>
-                   <Link to='/resetpassword/12346'><small> reset page </small></Link>
+                   
                         <div style={{textAlign:"right"}}>
-                        <button className="btn btn-secondary">Login</button>
+                        <button className="btn btn-secondary" onClick={getuser}>Login</button>
                         <p>New user? Sign up</p>
                         </div>
+             </div>
+        </div>
+        <div className="card" style={{width:"20rem",margin:"70px 0 0 400px",display:displaysuccess}}>
+             <div class="card-body">
+             <h4 class="card-title">Welcome to our website {authUser}</h4>
              </div>
         </div>
         </>
